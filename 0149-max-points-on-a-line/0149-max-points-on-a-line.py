69c1c9edc82097c7b6cc273a1a60f4940f5bb0aa
ETI SAI SIDDU PRANEETH <23p31a0486@acet.ac.in>
2026-06-08 13:30:22 +0530

    Time: 20 ms (91.77%), Space: 19.2 MB (89.73%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+class Solution:
+    def maxPoints(self, points):
+        n = len(points)
+
+        if n <= 2:
+            return n
+
+        ans = 0
+
+        for i in range(n):
+            slopes = defaultdict(int)
+
+            for j in range(i + 1, n):
+                x1, y1 = points[i]
+                x2, y2 = points[j]
+
+                # Vertical line
+                if x1 == x2:
+                    slope = "inf"
+                else:
+                    slope = (y2 - y1) / (x2 - x1)
+
+                slopes[slope] += 1
+
+            ans = max(ans, max(slopes.values(), default=0) + 1)
+
+        return ans

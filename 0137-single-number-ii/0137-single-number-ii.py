1c1f7009a728c9b855323811433e46a52466f6ee
ETI SAI SIDDU PRANEETH <23p31a0486@acet.ac.in>
2026-06-14 12:39:21 +0530

    Time: 28 ms (24.64%), Space: 20.4 MB (98.78%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def singleNumber(self, nums):
+        ans = 0
+
+        for bit in range(32):
+            count = 0
+
+            for num in nums:
+                if (num >> bit) & 1:
+                    count += 1
+
+            if count % 3:
+                ans |= (1 << bit)
+
+        # handle negative numbers
+        if ans >= 2**31:
+            ans -= 2**32
+
+        return ans
